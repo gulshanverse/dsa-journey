a49bd318e75d9270dd2eb14c67136d7da8eae77c
Gulshan Kumar <gulshankumar41309@gmail.com>
2026-05-28 20:50:50 +0530

    Added DSA basics programs

---
+++ DSA BASICS/1_io.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int x;
+    cout<<"Hello Gulshan"<<endl;
+    cout<<"How are you?"<<endl;
+    cout<<"How old are you?"<<endl;
+    cin>>x;
+    cout<<"I am "<< x <<" years old."<<endl;
+    return 0;
+}
---
+++ DSA BASICS/2_datatype.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //int
+    int x;
+    x = 36743;
+    cout<<x<<endl;
+    //float
+    float y;
+    y = 2893.3;
+    cout<<y<<endl;
+    //double
+    double z;
+    z = 3873.33;
+    cout<<z<<endl;
+    char ch;
+    ch = 'v';
+    cout<<ch<<endl;
+    string s;
+    s = "strong man become superman";
+    cout<<s<<endl;
+    long long l;
+    l = 23687.35678;
+    cout<<l<<endl;
+    return 0;
+}

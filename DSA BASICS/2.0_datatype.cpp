453abb609cd29153d15efef20c73989eeb6a51aa
Gulshan Kumar <gulshankumar41309@gmail.com>
2026-05-29 08:00:39 +0530

    New Changes

---
+++ DSA BASICS/2.0_datatype.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //float ,double
+    float x = 5.6;
+    float y = 5;
+    double z = 367.3892;
+    cout<<"Value of x: "<<x<<endl;
+    cout<<"Value of y: "<<y<<endl;
+    cout<<"Value of z: "<<z;
+    return 0;
+}
+
---
+++ DSA BASICS/2.2.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    ///int
+    int x = 10;
+    //long
+    long y = 15;
+    long long z = 150000000;
+    cout<<x<<endl<<y<<endl<<z;
+    return 0;
+}

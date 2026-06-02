a6f10d04fa5f72c7efa2724106d88926f5d21de6
Gulshan Kumar <gulshankumar41309@gmail.com>
2026-06-02 16:28:38 +0530

    IF ELSE STATEMENT

---
+++ DSA BASICS/4_0_ifelse.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    long long n;
+    cout<<"Enter the number: ";
+    cin>>n;
+    if (n % 2==0)
+    cout<<n<< " is an even number."<<endl;
+    else
+    cout<<n<< " is an odd number.";
+    return 0;
+}

84e07055db6a1fdd9de7a18a1e22612f47d55fa6
Gulshan Kumar <gulshankumar41309@gmail.com>
2026-06-27 23:55:38 +0530

    Set STL added

--- 3_STL/8_priorityQueue.cpp
+++ 3_STL/8_priorityQueue.cpp
@@ -1,15 +1,28 @@
 #include<bits/stdc++.h>
 using namespace std;
 void expalinPQ(){
-    priority_queue<int> pq;
-    pq.push(5);
-    pq.push(6);
-    pq.push(7);
-    pq.emplace(8);
+    priority_queue<int>pq;
 
-    cout<<pq.top();
-    pq.pop();
-    cout<<pq.top();
+   // Maximum Heap 
+    pq.push(5); // {5}
+    pq.push(2); // {5,2}
+    pq.push(8); // {8,5,2}
+    pq.emplace(10); // {10,8,5,2}
+
+    cout<<pq.top(); // prints 10
+    pq.pop(); // {8,5,2}
+    cout<<pq.top(); // prints 8
+
+    // Minimum Heap 
+    priority_queue<int, vector<int>,greater<int>> pq1;
+    pq1.push(5); //{5}
+    pq1.push(2); //{2,5}
+    pq1.push(8); // {2,5,8}
+    pq1.emplace(10); // {2,5,8,10}
+
+    cout<<pq1.top(); // prints 2
+    pq1.pop(); // {5,8,10}
+    cout<<pq1.top(); // prints 5
 
 }
 int main(){
